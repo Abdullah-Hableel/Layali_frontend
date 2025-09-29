926e381449127d0a8d7af33c20dc7fc2dd6d06b4
nawaf <nawafalrenezi@gmail.com>
2025-09-29 22:15:15 +0300

    done with the services i maight change the design tho

--- api/category.ts
+++ api/category.ts
@@ -1,6 +1,17 @@
 import instance from ".";
-
 export const getAllCategory = async () => {
-  const res = await instance.get(`/api/category`);
-  return res.data.category;
+  try {
+    const res = await instance.get("/api/category");
+    return res.data || [];
+  } catch (error) {
+    console.log("🚀 ~ getCategory  ~ error:", error);
+    return [];
+  }
 };
+export async function createCategory(name: string) {
+  const formData = new FormData();
+  formData.append("name", name);
+
+  const res = await instance.post("/api/category", formData, {});
+  return res.data.category;
+}
